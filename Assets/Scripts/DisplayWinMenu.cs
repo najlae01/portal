8dbda93fecf3bbeccd8b65c5a6147f86fc77d50c
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-06-23 21:46:51 +0100

    portal assignment

---
+++ Assets/Scripts/DisplayWinMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayWinMenu : MonoBehaviour {
+
+	public Text WinText;
+
+	// Use this for initialization
+	void Start () {
+        WinText.color = new Color(0, 0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+        WinText.color = new Color(1, 0, 0, 1);
+    }
+}
